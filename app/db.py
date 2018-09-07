0431bc8f2144ba820141a2e9d3ed2b711a867640
SilasKenneth <silaskenneth1@gmail.com>
2018-09-07 23:36:16 +0300

    [CHORE] Create the database to hold data for app

---
+++ app/db.py
@@ -0,0 +1,34 @@
+class Database(object):
+    def __init__(self):
+        self.users = {}
+        self.order_maps = {}
+        self.address_maps = {}
+        self.products = {}
+        self.emails = {}
+        self.user_ids = {}
+
+    def drop(self):
+        self.users = {}
+        self.order_maps = {}
+        self.address_maps = {}
+        self.products = {}
+
+    def create_order(self, order, user_id):
+        if len(self.users) == 0:
+            return False
+
+    def add_user(self, user):
+        """Method to add a user to the database"""
+        self.users.update({user.username: user})
+        self.emails.update({user.email: user.username})
+        self.user_ids.update({user.id : user.username})
+
+    def add_product(self, product):
+        """Method to add a product to our database"""
+        self.products.update({len(self.products) + 1: product})
+
+    def get_item(self, table):
+        return getattr(self, table, [])
+
+
+db = Database()
