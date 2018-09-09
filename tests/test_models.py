33f046d5a7f35a111b54a32212280784536e5b99
SilasKenneth <silaskenneth1@gmail.com>
2018-09-09 12:59:40 +0300

    [CHORE] Add more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import json
+
 from basetest import BaseTest
 from unittest import TestCase
 try:
@@ -12,13 +14,19 @@ class TestModel(BaseTest, TestCase):
     
     def test_can_add_user(self):
         random_user = "".join([random.choice(string.ascii_lowercase) for i in range(10)])
-        user_test = User(random_user, random_user + "gmail.com", "silas")
-        db.add_user(user_test)
-        user_after_add = User.get_by_email(user_test.email)
-        self.assertNotEqual(user_after_add, None)
-        self.assertEqual(user_after_add.username, user_test.username)
+        user_test = User(random_user, random_user + "@gmail.com", "silas")
+        try_post = self.client.post("/api/v1/users", data=json.dumps(user_test.json), content_type="application/json")
+        route = "/api/v1/users/"+str(user_test.id)
+        after_save = self.client.get("/api/v1/users/"+str(user_test.id), content_type="application/json")
+        tester = self.client.get("/api/v1/users/2", content_type="application/json")
+        print(tester.data)
+        self.assertEqual(user_test.username, user_test.username)
+        self.assertEqual(try_post.status_code, 200)
+        self.assertEqual(after_save.status_code, 200)
+        # print(after_save.data)
 
     def test_can_add_product_if_admin(self):
+        #This will be done after the auth module
         pass
 
     def test_can_delete_product(self):
@@ -40,7 +48,7 @@ class TestModel(BaseTest, TestCase):
     def test_can_add_address(self):
         address = self.silas_address
         self.silas.add_address(address)
-        self.assertEqual(1, 1)
+        #To be done
 
     def test_can_delete_address(self):
         pass
