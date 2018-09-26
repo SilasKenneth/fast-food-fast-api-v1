9664620f7efed6b74523b8ddd48ce20ffe5a4f95
SilasKenneth <silaskenneth1@gmail.com>
2018-09-26 15:05:16 +0300

    [FIX] Refactor the code to have put request

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -3,6 +3,7 @@ from app.db import db
 from app.models import Order, User, Product, Address
 from app.utils import empty
 
+
 class OrderResource(Resource):
     parser = reqparse.RequestParser()
 
@@ -33,87 +34,84 @@ class OrderResource(Resource):
                     res.append(order.json)
         return res, 200
 
-    def post(self, order_id=None):
+    def post(self):
         """A method to create a new order or modify an existing order"""
+        self.parser.add_argument("user_id", required=True,
+                                 help="You cannot place an order "
+                                      "anonymously")
+        self.parser.add_argument("address", required=True,
+                                 help="Please specify an address to ship "
+                                      "your order to")
+        self.parser.add_argument("items", required=True,
+                                 help="You cannot order nothing "
+                                      "please specify some items")
+        args = self.parser.parse_args()
+        user_id = args.get("user_id")
+        address = args.get("address")
+        items = args.get("items")
+        if empty(user_id) or empty(address) or empty(items):
+            return {"ok": False, "code": "400",
+                    "message": "Please make sure you have no missing "
+                               "requirements for an order"}, 400
+        def checker0(y):
+            if isinstance(y, str):
+                return None
+            if not y.isnumeric():
+                return None
+            return User.get_by_id(int(y))
+        user = User.get_by_id(int(user_id))
+        if user is None:
+            return {"ok": False, "code": "400",
+                    "message": "Could not recognize the person who's "
+                               "trying to place the order"}, 400
+        def checker(y):
+            if isinstance(y, str):
+                return None
+            if not y.isnumeric():
+                return None
+            return Address.find_by_id(int(y))
+        add = checker(address)
+        if add is None:
+            return {"ok": False, "code": "404",
+                    "message": "Could not find the specified address"
+                               " or you specified an address with an"
+                               " invalid format "
+                               "first add an address"}, 400
+        items = items.split(",")
+        if not items:
+            return {"ok": False, "code": 403, "message": "Please specify"
+                                                         "items to complete the request"}, 403
+        def found(y):
+            if not isinstance(y, str):
+                return None
+            if not str(y).isnumeric():
+                return None
+            return Product.get_by_id(int(y))
 
-        if order_id is None or order_id == 0:
-            self.parser.add_argument("user_id", required=True,
-                                     help="You cannot place an order "
-                                          "anonymously")
-            self.parser.add_argument("address", required=True,
-                                     help="Please specify an address to ship "
-                                          "your order to")
-            self.parser.add_argument("items", required=True,
-                                     help="You cannot order nothing "
-                                          "please specify some items")
-            args = self.parser.parse_args()
-            user_id = args.get("user_id")
-            address = args.get("address")
-            items = args.get("items")
-            if empty(user_id) or empty(address) or empty(items):
-                return {"ok": False, "code": "400",
-                        "message": "Please make sure you have no missing "
-                                   "requirements for an order"}, 400
-            user = User.get_by_id(int(user_id))
-            if user is None:
-                return {"ok": False, "code": "400",
-                        "message": "Could not recognize the person who's "
-                                   "trying to place the order"}, 400
-            add = Address.find_by_id(int(address))
-            print(add)
-            if add is None:
-                return {"ok": False, "code": "404",
-                        "message": "Could not find the specified address "
-                                   "first add an address"}, 400
-            items = items.split(",")
-
-            def found(y):
-                return Product.get_by_id(int(y))
-
-            products = [found(item) for item in items if item is not None]
-            products = [x for x in products if x is not None]
-            prods = [product for product in products]
-            if not prods:
-                return {"code": "404", "ok": False,
-                        "message": "The products you specified don't exist or "
-                                   "you just never specified any please "
-                                   "check again"}, 400
-            order = Order(db.users[user].id, add)
-            total = 0.00
-            for p in prods:
-                total += float(p.unit_price)
-            order.items = prods
-            order.total = total
-            db.order_maps.update({order.id: user})
-            db.users[user].orders.append(order)
-            return {"code": 200, "ok": True,
-                    "message": "You successfully placed "
-                               "the order thank you", "data": order.json}
-        else:
-            parser = reqparse.RequestParser()
-            parser.add_argument("status", required=True,
-                                help="Please specify whether to accept, "
-                                     "reject or mark order as complete")
-            args = parser.parse_args()
-            status = args.get("status")
-            if len(status.strip()) == 0:
-                return {"code": 400, "ok": False,
-                        "message": "Please specify a valid status"}, 400
-            order = db.order_maps.get(order_id)
-            if order is None:
-                return {"code": "404", "ok": False,
-                        "message": "Couldn't find the order specified "
-                                   "try another"}, 400
-            for i in range(len(db.users[order].orders)):
-                if db.users[order].orders[i] is not None:
-                    if db.users[order].orders[i].id == order_id:
-                        db.users[order].orders[i].status = status
-                        return {"code": 200, "ok": True,
-                                "message": "The order was successfully "
-                                           "updated",
-                                "data": db.users[order].orders[i].json}, 200
-            return {"code": 400, "ok": False,
-                    "message": "Could not find the order specified"}, 400
+        products = [found(item) for item in items if item is not None]
+        products = []
+        for item in items:
+            if found(item) is None:
+                return {"code": 404, "ok": False, "message": "The menu item with the id %s does not exist" % item}, 404
+            products.append(found(item))
+        products = [x for x in products if x is not None]
+        prods = [product for product in products]
+        if not prods:
+            return {"code": "404", "ok": False,
+                    "message": "The products you specified don't exist or "
+                               "you just never specified any please "
+                               "check again"}, 400
+        order = Order(db.users[user].id, add, items)
+        total = 0.00
+        for p in prods:
+            total += float(p.unit_price)
+        order.items = prods
+        order.total = total
+        db.order_maps.update({order.id: user})
+        db.users[user].orders.append(order)
+        return {"code": 200, "ok": True,
+                "message": "You successfully placed "
+                           "the order thank you", "data": order.json}
 
     def delete(self, order_id):
         """Cancel an order with a delete request"""
@@ -132,3 +130,31 @@ class OrderResource(Resource):
         return {"code": 400, "ok": False,
                 "message": "Could not find the order specified. "
                            "Please try another"}, 400
+
+    def put(self, order_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("status", required=True,
+                            help="Please specify whether to accept, "
+                                 "reject or mark order as complete")
+        args = parser.parse_args()
+        status = args.get("status")
+        if len(status.strip()) == 0:
+            return {"code": 400, "ok": False,
+                    "message": "Please specify a valid status"}, 400
+        if status not in ["pending", "accepted", "complete", "rejected"]:
+            return {"message": "Please specify a valid status statuses are complete, pending, rejected or accepted"}
+        order = db.order_maps.get(order_id)
+        if order is None:
+            return {"code": "404", "ok": False,
+                    "message": "Couldn't find the order specified "
+                               "try another"}, 400
+        for i in range(len(db.users[order].orders)):
+            if db.users[order].orders[i] is not None:
+                if db.users[order].orders[i].id == order_id:
+                    db.users[order].orders[i].status = status
+                    return {"code": 200, "ok": True,
+                            "message": "The order was successfully "
+                                       "updated",
+                            "data": db.users[order].orders[i].json}, 200
+        return {"code": 400, "ok": False,
+                "message": "Could not find the order specified"}, 400
